a5808736478e7301c31e81f4f0f495dc53baa3a6
Santiago Ramirez <ixtlitonmre@gmail.com>
2026-07-25 18:34:24 -0500

    Add an example of the enumeration function

---
+++ EtCetera/majors_enum.py
@@ -0,0 +1,9 @@
+def main():
+    majors = ["Mathematics", "Physics", "Computer Science"]
+
+    for i, major in enumerate(majors):
+        #Enumaretes the content of the majors list, starts with index 0
+        print(i + 1, major)
+
+if __name__ == "__main__":
+    main()
